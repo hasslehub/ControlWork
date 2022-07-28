94ed1500eb2d81d549a550b7992fb8ed04d0cbc5
Ivan <hassled@mail.ru>
2022-07-28 23:23:51 +0300

    Decision

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FindFromArray(string[] array)
+{
+    string[] finalArray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            finalArray[i] = array[i];
+        }
+    }
+    Console.Write("Результат: ");
+    PrintArray(finalArray);
+}
+
+void PrintArray(string[] currentArray)
+{
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        Console.Write($"{currentArray[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] mainArray = { "hello", "2", "world", ":-)" };
+Console.WriteLine();
+Console.Write("Первоначальный массив: ");
+PrintArray(mainArray);
+
+FindFromArray(mainArray);
+Console.WriteLine();
+
